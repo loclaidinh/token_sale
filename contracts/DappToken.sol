4da3da646b6da753bb41ee755059a0cbfe24a7c9
loclaidinh <42854274+loclaidinh@users.noreply.github.com>
2018-09-07 08:10:55 +0700

    2. transfer token

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -1,12 +1,50 @@
 pragma solidity ^0.4.23;
 
 contract DappToken {
+	// Name
+	// Symbol
+
+	string public name = "DApp Token";
+	string public symbol = "DAT";
+	string public standard = "DApp Token v1.0";
+
+	event Transfer(
+		address indexed _from,
+		address indexed _to,
+		uint256 _value
+	);
+
 	// Constructor
 	// Set the total number of tokens
 	// Read the total number of tokens
 	uint256 public totalSupply;
 
-	constructor() public {
-		totalSupply = 1000000;
+	mapping(address => uint256) public balanceOf;
+
+	constructor(uint256 _initialSupply) public {
+		balanceOf[msg.sender] = _initialSupply;
+		totalSupply = _initialSupply;
+		// allocate the _initialSupply
+	}
+
+	// transfer
+
+
+	function transfer(address _to, uint256 _value) public returns (bool success){
+		// exception if account doesn't have enough
+		require(balanceOf[msg.sender] >= _value);
+		// transfer the balance
+
+		balanceOf[msg.sender] -= _value;
+		balanceOf[_to] += _value;
+		// transfer event
+
+		emit Transfer(msg.sender, _to, _value);
+
+		// return a boolean
+		
+		return true;
+
+
 	}
 }
